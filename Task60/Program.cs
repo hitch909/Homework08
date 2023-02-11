6810c98b906c4097243131022443c2962283c1f8
hitch909 <evgenij909@yandex.ru>
2023-02-11 21:17:03 +0500

    60 задача готова

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.  
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.Clear();
+int[,,] array3D = new int[2, 2, 2];
+FillArray(array3D);
+PrintIndex(array3D);
+
+void FillArray(int[,,] arr)    // такое решение списал из интернета, понравилось.
+{
+    int number = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += number;
+                number += 5;
+            }
+        }
+    }
+}
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]} ({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
